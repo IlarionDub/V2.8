13234043aca236425adbf2761bc7aab47517319b
IlarionDub <ilariondub5@gmail.com>
2024-12-03 22:26:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -719,7 +719,7 @@ function updateUserUI() {
         loginButton?.classList.add("hidden");
         logoutButton?.classList.remove("hidden");
         loggedInUserSpan?.classList.remove("hidden");
-        loggedInUserSpan.innerText = `Logged in as: ${loggedInUser}`;
+        loggedInUserSpan.innerText = `Logged in as: ${loggedInUser.name}`;
 
     } else {
         loginButton?.classList.remove("hidden");
